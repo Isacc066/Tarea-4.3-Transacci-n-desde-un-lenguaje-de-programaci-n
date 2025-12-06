223a032c8f65f0640548ae3d47f25862caf45043
Isacc066 <s23120207@alumnos.itsur.edu.mx>
2025-12-05 18:41:28 -0600

    Agregar archivos de proyecto.

---
+++ Tarea 4.3 Transacción desde un lenguaje de programación/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tarea_4._3_Transacción_desde_un_lenguaje_de_programación
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
